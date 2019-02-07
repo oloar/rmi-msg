c0c457f128e00acfb03e54a31c167f19d409c287
Gaetan Sorin <contact@gaetansorin.com>
2019-02-07 14:27:12 +0100

    Correct indentation

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -2,25 +2,25 @@ import java.rmi.*;
 import java.rmi.registry.*;
 
 public class ChatClient {
-  public static void main(String [] args) {
+	public static void main(String [] args) {
 
-	try {
-	  if (args.length < 1) {
-	   System.out.println("Usage: java HelloClient <rmiregistry host>");
-	   return;}
+		try {
+			if (args.length < 1) {
+				System.out.println("Usage: java HelloClient <rmiregistry host>");
+				return;}
 
-	String host = args[0];
+			String host = args[0];
 
-	// Get remote object reference
-	Registry registry = LocateRegistry.getRegistry(host);
-	Hello h = (Hello) registry.lookup("HelloService");
+			// Get remote object reference
+			Registry registry = LocateRegistry.getRegistry(host);
+			Hello h = (Hello) registry.lookup("HelloService");
 
-	// Remote method invocation
-	String res = h.sayHello();
-	System.out.println(res);
+			// Remote method invocation
+			String res = h.sayHello();
+			System.out.println(res);
 
-	} catch (Exception e)  {
-		System.err.println("Error on client: " + e);
+		} catch (Exception e)  {
+			System.err.println("Error on client: " + e);
+		}
 	}
-  }
 }
--- src/ChatServer.java
+++ src/ChatServer.java
@@ -5,21 +5,21 @@ import java.rmi.registry.*;
 
 public class ChatServer {
 
-  public static void  main(String [] args) {
-	  try {
-		  // Create a Hello remote object
-	    HelloImpl h = new HelloImpl ("Hello world !");
-	    Hello h_stub = (Hello) UnicastRemoteObject.exportObject(h, 0);
+	public static void  main(String [] args) {
+		try {
+			// Create a Hello remote object
+			HelloImpl h = new HelloImpl ("Hello world !");
+			Hello h_stub = (Hello) UnicastRemoteObject.exportObject(h, 0);
 
-	    // Register the remote object in RMI registry with a given identifier
-	    Registry registry= LocateRegistry.getRegistry();
-	    registry.rebind("HelloService", h_stub);
+			// Register the remote object in RMI registry with a given identifier
+			Registry registry= LocateRegistry.getRegistry();
+			registry.rebind("HelloService", h_stub);
 
-	    System.out.println ("Server ready");
+			System.out.println ("Server ready");
 
-	  } catch (Exception e) {
-		  System.err.println("Error on server :" + e) ;
-		  e.printStackTrace();
-	  }
-  }
+		} catch (Exception e) {
+			System.err.println("Error on server :" + e) ;
+			e.printStackTrace();
+		}
+	}
 }
