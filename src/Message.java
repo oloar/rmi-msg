a99812fa2fc32e83ad034891cd57f647ffd21025
Bastien Zigmann <bastien.zigmann@free.fr>
2019-02-07 15:13:10 +0100

    Ajout de l'id dans le message

--- src/Message.java
+++ src/Message.java
@@ -1,9 +1,11 @@
 public class Message {
 	private String sender;
+	private int senderId;
 	private String text;
 
-	public Message(String sender, String text) {
+	public Message(String senderId, String sender, String text) {
 		this.sender = sender;
+		this.senderId = senderId;
 		this.text = text;
 	}
 
@@ -13,5 +15,9 @@ public class Message {
 
 	public String text() {
 		return this.text;
+	}
+
+	public int senderId(){
+		return this.senderId;
 	}
 }
