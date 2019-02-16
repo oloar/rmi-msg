078b2a9ee494ed2dc9c5d8be245cb6319e56ddc6
Bastien Zigmann <bastien.zigmann@xsalto.com>
2019-02-16 20:21:27 +0100

    synchro du server pour éviter les accès concurents

--- src/ServImplem.java
+++ src/ServImplem.java
@@ -22,7 +22,7 @@ public class ServImplem implements Serv {
 	 * @param c : client reference
 	 * @return : client id strictly greater than 0
 	 */
-	public int clientRegister(Client c) throws RemoteException {
+	public synchronized int clientRegister(Client c) throws RemoteException {
 		int cId;
 
 		id++; // min id = 1
@@ -36,7 +36,7 @@ public class ServImplem implements Serv {
 	 * Client leaves a room
 	 * @param c : Client
 	 */
-	public void clientLeave(Client c) throws RemoteException {
+	public synchronized void clientLeave(Client c) throws RemoteException {
 		for(Room r : getRooms())
 			if(r.isInTheRoom(c)) {
 				r.leaveRoom(c);
@@ -51,7 +51,7 @@ public class ServImplem implements Serv {
 	 * Send message to server
 	 * @param m : message to send
 	 */
-	public void sendMsgToServ(Message m) throws RemoteException {
+	public synchronized void sendMsgToServ(Message m) throws RemoteException {
 		System.out.println(m.sender() + " says " + m.text() + " in " + m.roomId());
 
 		sendMsgToRoom(m, Objects.requireNonNull(getRoomById(m.roomId())));
@@ -64,7 +64,7 @@ public class ServImplem implements Serv {
 	 * @param m : message to send
 	 * @param room : room to send it to
 	 */
-	private void sendMsgToRoom(Message m, Room room){
+	private synchronized void sendMsgToRoom(Message m, Room room){
 		for(Client c : room.getClients()){
 			try{
 				if (c.getId() != m.senderId()) {
@@ -81,7 +81,7 @@ public class ServImplem implements Serv {
 	 * Send message to all connected clients
 	 * @param m : message to send
 	 */
-	private void broadcast(Message m) {
+	private synchronized void broadcast(Message m) {
 		for (Room r : rooms){
 			for (Client c : r.getClients())
 				try {
@@ -98,7 +98,7 @@ public class ServImplem implements Serv {
 	/////////////////////////////////////////////////////////////////////////////////////
 	// HISTORY
 	/////////////////////////////////////////////////////////////////////////////////////
-	private void writeToHistory(Message m) {
+	private synchronized void writeToHistory(Message m) {
 		if (m.senderId() != 0) { // Only log clients message
 			try {
 				BufferedWriter br = new BufferedWriter(new FileWriter("history.txt", true));
@@ -115,7 +115,7 @@ public class ServImplem implements Serv {
 	 * Construct an arraylist of messages from a file
 	 * @return The history in the form a an arraylist of message
 	 */
-	public ArrayList<Message> getHistory(Client c) throws RemoteException {
+	public synchronized ArrayList<Message> getHistory(Client c) throws RemoteException {
 		ArrayList<Message> history = new ArrayList<>();
 		BufferedReader br;
 		String line;
@@ -149,7 +149,7 @@ public class ServImplem implements Serv {
 	/**
 	 * Diplay rooms information
 	 */
-	public String printRooms() throws RemoteException{
+	public synchronized String printRooms() throws RemoteException{
 		StringBuilder res = new StringBuilder();
 		for(Room r : this.rooms){
 			res.append("Room ").append(r.getId()).append(" has ").append(r.getNumberClients()).append(" clients connected.\n");
@@ -162,7 +162,7 @@ public class ServImplem implements Serv {
 	 * @param c : client
 	 * @param newRoomId : the room to switch to
 	 */
-	public void changeRoom(Client c, int newRoomId) throws RemoteException{
+	public synchronized void changeRoom(Client c, int newRoomId) throws RemoteException{
 		Room oldRoom, newRoom;
 		if(existingId(newRoomId)){
 			oldRoom = Objects.requireNonNull(getRoomById(getRoomOfClient(c)));
